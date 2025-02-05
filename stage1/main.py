937d1eb18d7e37394a82abf97c9f18434c1c9435
matutozi <sobowalegz2@gmail.com>
2025-02-05 14:30:07 +0100

    handle CORS

--- stage1/main.py
+++ stage1/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import httpx
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"] 
+)
 
 def is_prime(n):
     """method that checks if n is prime"""
